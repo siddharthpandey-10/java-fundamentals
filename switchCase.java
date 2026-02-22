a7380f23363afcce41056bdab278aa66f53368f2
Siddharth <officialsiddharth.pandey@gmail.com>
2026-02-22 12:22:02 +0530

    java basics

---
+++ switchCase.java
@@ -0,0 +1,9 @@
+public class switchCase {
+    public static void main(String[] args) {
+        //equal comparison
+        //menu driven program
+        //char, byte, short, int, String
+        //jump table concept
+        //expression 
+    }
+}
